7ac8c244d7ee73da832a8a65c8d5e89de2dfe630
Marcel Pinto <p.marcel.augusto@gmail.com>
2026-08-28 13:56:07 -0300

    Adding rich to enhance print

---
+++ rich/rich001.py
@@ -0,0 +1,7 @@
+from rich import print
+
+print("Hello [red]World![/red] :earth_americas:")
+
+print("Olá, [bold blue on white]Pequeno Gafanhoto[/] :vulcan_salute:")
+
+print(":+1: :martial_arts_uniform: :-1:")
