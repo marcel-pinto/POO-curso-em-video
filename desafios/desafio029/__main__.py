c2a99f110ac5f1bcb19382e4c11751571b8f9700
Marcel Pinto <p.marcel.augusto@gmail.com>
2026-09-09 20:28:21 -0300

    Gabarito desafio 29

--- desafios/desafio029/__main__.py
+++ desafios/desafio029/__main__.py
@@ -1,10 +1,18 @@
 from diario import Diario
+from getpass import getpass
+from rich import print
 
 def main():
     d = Diario()
     d.escrever("Consegui fazer o exercicio")
     d.escrever("Não sou carioca")
-    d.ler()
 
+    try:
+        d.ler()
+    except Exception as error:
+        print(f"[red] Erro: {error}")
+
+    # novasenha = getpass("Digite a nova senha: ")
+    # d.senha = novasenha
 if __name__ == "__main__":
     main()
--- desafios/desafio029/diario.py
+++ desafios/desafio029/diario.py
@@ -4,19 +4,33 @@ from getpass import getpass
 class Diario:
     def __init__(self, senha = "CeV!@"):
         self.__segredos = []
-        self.__senha = senha
+        self.__senha = senha.strip()
 
 
     def escrever(self, msg):
-        self.__segredos.append(msg)
+        if isinstance(msg, str) and len(msg) > 0:
+            self.__segredos.append(msg.strip())
 
     def ler(self, senha = None):
         if not senha:
             senha = getpass("Por favor digite sua senha: ")
 
         if senha != self.__senha:
-            print("Senha inválida!")
+            raise PermissionError("Senha inválida! Você não pode ler meu diário!")
         else:
             print("[green]DIARIO LIBERADO[/]")
             for segredo in self.__segredos:
                 print(f"- {segredo}")
+
+    @property
+    def senha(self):
+        raise PermissionError("Ninguém tem permissão de ver a senha.")
+
+    @senha.setter
+    def senha(self, novasenha):
+        senha = getpass("Digite a senha antiga: ")
+        if senha == self.__senha:
+            self.__senha = senha
+            print("Senha alterada com sucesso.")
+        else:
+            print("A senha antiga não está correta.")
