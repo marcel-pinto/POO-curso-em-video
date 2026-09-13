8f9c8363f8d64662cfdd8a8e1dadd29278be1d4d
Marcel Pinto <p.marcel.augusto@gmail.com>
2026-09-12 23:21:02 -0300

    Gabarito desafio 35

--- desafios/desafio035/arquivos.py
+++ desafios/desafio035/arquivos.py
@@ -1,22 +1,38 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class Arquivo(ABC):
-    def __init__(self, nome:str, extensao:str, tamanho:int):
+    def __init__(self, nome:str, extensao:str, tamanho:int = 0):
         self.nome = nome
-        self._extensao = extensao
+        self._extensao = None
         self.tamanho = tamanho
+        self.extensao = extensao
 
+    @property
+    def extensao(self):
+        return self._extensao
+
+    @extensao.setter
+    def extensao(self, ext:str):
+        formatos = ["pdf", "doc", "docx"]
+        ext = ext.lower().strip()
+
+        if ext in formatos:
+            self._extensao = ext
+        else:
+            raise AttributeError("O arquivo está em um formato não suportado")
+        
     @property
     def nome_completo(self):
         return f"'{self.nome}' ({self.tamanho / 1_000_000:.2f} MB)"
 
+    @abstractmethod
     def abrir(self):
         pass
 
 
 class PDF(Arquivo):
     extensao = "pdf"
-    def __init__(self, nome, tamanho):
+    def __init__(self, nome:str, tamanho:int):
         super().__init__(nome, PDF.extensao, tamanho)
 
     def abrir(self):
@@ -24,7 +40,7 @@ class PDF(Arquivo):
 
 class DOC(Arquivo):
     extensao = "docx"
-    def __init__(self, nome, tamanho):
+    def __init__(self, nome:str, tamanho:int):
         super().__init__(nome, DOC.extensao, tamanho)
 
     def abrir(self):
